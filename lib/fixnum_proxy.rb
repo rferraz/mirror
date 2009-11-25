fd4ab6733e67e5f59198f4fe2e6a07a0023f8346
Ronaldo Ferraz <ronaldoferraz@gmail.com>
2009-11-25 00:35:52 -0200

    Added new methods to the integer proxy

--- lib/fixnum_proxy.rb
+++ lib/fixnum_proxy.rb
@@ -10,4 +10,18 @@ class FixnumProxy < Proxy
     self
   end
   
+  def sqrt
+    Math.sqrt(delegate)
+  end
+  
+  def compare_to(other)
+    if self < other
+      "lt"
+    elsif self > other
+      "gt"
+    else
+      "eq"
+    end
+  end
+  
 end
